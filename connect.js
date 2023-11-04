2c1ff8974e9794001a44b16ecf73ee443c151f8d
NABILAFZAL19 <nabilafzal19@gmail.com>
2023-11-05 00:55:45 +0530

    added changes in mongoose.connect for version 6

--- connect.js
+++ connect.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 exports.mongodbConnection = async (url) => {
   try {
-    await mongoose.connect(url);
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("mongodb connection successful");
   } catch (error) {
     console.log(error);
